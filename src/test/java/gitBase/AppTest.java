dd1d6a47d744a35ec6532efe313e49328e26898c
Rajanikanta Kamila <Rajanikanta Kamila@LAPTOP-TKMEP63N>
2022-03-08 10:25:30 +0530

    Package Rename

--- src/test/java/gitBase/AppTest.java
+++ src/test/java/gitBase/AppTest.java
@@ -1,4 +1,4 @@
-package com.GitDemo;
+package gitBase;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
--- src/test/java/gitFlipkart/FlipkartGitDemo.java
+++ src/test/java/gitFlipkart/FlipkartGitDemo.java
@@ -1,4 +1,4 @@
-package com.qa.git.practice;
+package gitFlipkart;
 
 import java.util.ArrayList;
 
